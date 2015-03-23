7c270cdf4039ce920aa12b17ee0fa7f099ef04dd
aotta <andrea.ottaviani.69@gmail.com>
2015-03-23 16:17:40 +0100

    Separate layer for minutes

--- src/simplicity.c
+++ src/simplicity.c
@@ -14,9 +14,7 @@ static void line_layer_update_callback(Layer *layer, GContext* ctx) {
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   // Need to be static because they're used by the system later.
   // static char s_time_text[] = "00:00";
-  static char s_time_text[] = "00";
-  
-  // minuters change here!!!!
+  static char s_hour_text[] = "00:";
   static char s_min_text[] = "00";
   
   static char s_date_text[] = "Xxxxxxxxx 00";
@@ -24,7 +22,6 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 
   strftime(s_date_text, sizeof(s_date_text), "%B %e", tick_time);
   text_layer_set_text(s_date_layer, s_date_text);
-
     
     // secondi lampeggianti
   time_t now = time(NULL);
@@ -37,42 +34,43 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   //  time_format = "%R";
   //} else {
     if (pari == 0) {
-    time_format = "%I:%M";  
+    time_format = "%I:";  
     } else {
-       time_format = "%I %M";
+       time_format = "%I ";
     } 
   //}
-  
-  
-  strftime(s_time_text, sizeof(s_time_text), time_format, tick_time);
-
+    
+  strftime(s_hour_text, sizeof(s_hour_text), time_format, tick_time);
   
   // Handle lack of non-padded hour format string for twelve hour clock.
-  if (!clock_is_24h_style() && (s_time_text[0] == '0')) {
-    memmove(s_time_text, &s_time_text[1], sizeof(s_time_text) - 1);
+  if (!clock_is_24h_style() && (s_hour_text[0] == '0')) {
+    memmove(s_hour_text, &s_hour_text[1], sizeof(s_hour_text) - 1);
   }
-   
   
-  text_layer_set_text(s_time_layer, s_time_text);
+  text_layer_set_text(s_time_layer, s_hour_text);
+  
+  // setta i minuti
+  strftime(s_min_text, sizeof(s_min_text), "%M", tick_time);
+  text_layer_set_text(s_minutes_layer, s_min_text);
+  
 }
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
 
-  // s_date_layer = text_layer_create(GRect(8, 28, 136, 80));
-  s_date_layer = text_layer_create(GRect(8, 18, 136, 30));
+  // Imposta il layer per la data
+  s_date_layer = text_layer_create(GRect(8, 38, 136, 30));
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_background_color(s_date_layer, GColorClear);
   
-   
    // custom fonts!
-     
   s_custom_font_12 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LCD_12));
     
   text_layer_set_font(s_date_layer, s_custom_font_12);
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
-  s_time_layer = text_layer_create(GRect(7, 42, 147, 126));
+  // imposta il layer per l'ora
+  s_time_layer = text_layer_create(GRect(7, 62, 147, 106));
   text_layer_set_text_color(s_time_layer, GColorWhite);
  
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -81,6 +79,15 @@ static void main_window_load(Window *window) {
     
   text_layer_set_font(s_time_layer, s_custom_font_60);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+  // imposta il layer per i minuti 
+  s_minutes_layer = text_layer_create(GRect(87, 62, 67, 106));
+  
+  text_layer_set_text_color(s_minutes_layer, GColorWhite);
+  text_layer_set_background_color(s_minutes_layer, GColorClear);
+  text_layer_set_font(s_minutes_layer, s_custom_font_60);
+  layer_add_child(window_layer, text_layer_get_layer(s_minutes_layer));
+
 
   // GRect line_frame = GRect(8, 37, 139, 39);
   GRect line_frame = GRect(8, 30, 139, 2);
@@ -93,6 +100,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_minutes_layer);
+  
   fonts_unload_custom_font(s_custom_font_12);
   fonts_unload_custom_font(s_custom_font_60);
   layer_destroy(s_line_layer);
